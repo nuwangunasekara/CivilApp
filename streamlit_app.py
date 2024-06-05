719a1254b329c8855b2dad1dfd164fc629bc5b1f
nuwangunasekara <ng198002@gmail.com>
2024-06-05 18:38:26 +1200

    Change colour scheme

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,19 +17,19 @@ ic1,ic2=input_container.columns(2)
 
 
 with ic1:
-    st.write(f"**Axial-capacity predictor**")
+    st.subheader('Axial-capacity predictor', divider='rainbow')
+
+    dynamic_label_placeholder = st.empty()
+
     fy=st.number_input("**fy:**",min_value=0.0,max_value=1000.0,step=1.0,value=436.0)
     t=st.number_input("**t:**",min_value=0.0,max_value=1000.0,step=1.0,value=1.92)
     L=st.number_input("**L:**",min_value=0.0,max_value=3000.0,step=1.0,value=2000.0)
-with ic2:
     d1=st.number_input("**D1:**", min_value=0.0,max_value=2000.0,step=1.0,value=359.2)
     d2=st.number_input("**D2:**", min_value=0.0,max_value=1000.0,step=1.0,value=128.0)
     d3 = st.number_input("**D3:**", min_value=0.0, max_value=1000.0, step=1.0, value=24.7)
     d12 = st.number_input("**D12:**", min_value=0.0, max_value=1000.0, step=1.0, value=7.68)
     d13 = st.number_input("**D13:**", min_value=0.0, max_value=1000.0, step=1.0, value=7.68)
 
-
-with ic2:
     x = sample_x.copy()
 
     x['fy'] = fy
@@ -43,4 +43,4 @@ with ic2:
 
     pred = automl.model.predict(x)
 
-    st.write(f"**axial-capacity** = **{pred[0]:3}**")
+    dynamic_label_placeholder.write(f"**Axial-capacity for below values** = **{round(pred[0], 1)}**")
